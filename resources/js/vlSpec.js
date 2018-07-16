182a8d66637200ae269341ae0d76333467fa7c64
Jairo Ruiz Saenz <jairoruizsaenz@gmail.com>
2018-07-15 20:22:39 -0500

    Upload files
    Upload files

---
+++ resources/js/vlSpec.js
@@ -0,0 +1,5 @@
+// Assign the specification to a local variable vlSpec.
+const vlSpec = "resources/json/Camara2018.json";
+
+// Embed the visualization in the container with id `vis`
+vegaEmbed("#vis", vlSpec);
